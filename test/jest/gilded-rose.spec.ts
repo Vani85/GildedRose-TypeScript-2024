1ad581158e656ec33a8c83ccb01473042bcf2d1a
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-01-30 15:16:41 +0000

    removed comments

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,11 +1,6 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  // it('should foo', () => {
-  //   const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].name).toBe('fixme');
-  // });
 
   it('Once the sell by date has passed, Quality degrades twice as fast', () => {
     const gildedRose = new GildedRose([new Item('Bread', 0, 10)]);
